d06e0e9f63eb969fcade5b9d8401fee4b0caa2ba
khinhtetmoe <khinkhinhtet65@gmail.com>
2021-05-20 13:20:27 +0900

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('content')
+    <h3>Contact Page</h3>
+    @foreach($data as $key=>$value)
+        {{$key .' = '. $value}}
+    @endforeach
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>AP Laravel</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+
+<h2 style="text-align:center">Sample Blog Site</h2>
+<body>
+    @yield('content')
+</body>
+</html>
