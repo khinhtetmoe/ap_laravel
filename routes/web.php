6c46e047fb76640339dcac045155a549076e9a7b
khinhtetmoe <khinkhinhtet65@gmail.com>
2021-05-22 14:32:07 +0900

    week4 started

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',[HomeController::class,'index']);
+//Route::get('/',[HomeController::class,'index']);
+Route::resource('posts', HomeController::class);
