810d79cff055e412da0d68c8e594be0cf7fdab9b
khinhtetmoe <khinkhinhtet65@gmail.com>
2021-05-31 01:24:58 +0900

    compiling assets

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <head>
 <title>AP Laravel</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!-- <link rel="stylesheet" href="css/app.css"> -->
 </head>
 
 <h2 style="text-align:center">Sample Blog Site</h2>
